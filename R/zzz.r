458f89f98ff4e5af469921fc4b76fc4dcd36adbe
JED <jedazard@users.noreply.github.com>
2015-03-03 14:58:22 -0500

    R Package R Codes

--- R/zzz.r
+++ R/zzz.r
@@ -1,9 +1,9 @@
 ##########################################################################################################################################
-# PrimSRC
+# PRIMsrc
 ##########################################################################################################################################
 
 .onAttach <- function(libname, pkgname) {
     SSver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), fields="Version")
     packageStartupMessage(paste(pkgname, SSver))
-    packageStartupMessage("Type PrimSRC.news() to see new features, changes, and bug fixes\n")
+    packageStartupMessage("Type PRIMsrc.news() to see new features, changes, and bug fixes\n")
 }
