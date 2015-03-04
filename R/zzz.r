98964ab6a36880839074f8139759a1d67fb59de6
JED <jedazard@users.noreply.github.com>
2015-03-04 01:51:54 -0500

    Deletions

--- R/zzz.r
+++
@@ -1,9 +0,0 @@
-##########################################################################################################################################
-# PRIMsrc
-##########################################################################################################################################
-
-.onAttach <- function(libname, pkgname) {
-    SSver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), fields="Version")
-    packageStartupMessage(paste(pkgname, SSver))
-    packageStartupMessage("Type PRIMsrc.news() to see new features, changes, and bug fixes\n")
-}
